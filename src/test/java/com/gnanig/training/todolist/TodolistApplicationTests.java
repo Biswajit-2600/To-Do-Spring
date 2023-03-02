0fe748347c28547503026e14209ec0b026727a76
Biswajit_2600 <biswajitpanda754@gmail.com>
2023-03-02 20:02:09 +0530

    modified unit tests

--- src/test/java/com/gnanig/training/todolist/TodolistApplicationTests.java
+++ src/test/java/com/gnanig/training/todolist/TodolistApplicationTests.java
@@ -1,32 +1,28 @@
 package com.gnanig.training.todolist;
 
+
 import com.gnanig.training.todolist.RequestDataBody.DateTaskData;
 import com.gnanig.training.todolist.database.MyToDoListData;
+import com.gnanig.training.todolist.services.MyToDoList;
 import com.gnanig.training.todolist.services.MyToDoListImpl;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.junit.BeforeClass;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-@SpringBootTest
 class TodolistApplicationTests {
 
-    MyToDoListImpl obj = new MyToDoListImpl();
-
-    @BeforeClass
-    public static void dataLoad()
-    {
-        MyToDoListData data = MyToDoListData.getInstance();
-        List<DateTaskData> toDolist = data.getToDo();
-    }
+    MyToDoList obj = new MyToDoListImpl();
+    DateTaskData dateTask = new DateTaskData();
 
     @Test
-    void contextLoads() {
-        obj.create("23.12.31","complete unit testing");
-        obj.read();
+    void dateTaskTest() {
+//        dateTask.setTask("complete unit testing");
+//        dateTask.setDate("23.12.31");
+//        dateTask.setCompletionStatus("not completed");
+//        obj.create("23.12.31", "complete unit testing");
+        Assertions.assertEquals("[]",obj.read());
     }
 
 }
